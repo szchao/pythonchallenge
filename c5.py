44acf820e0fccba852d53a4ce7d87698dfbe001e
szchao <suzichao@gmail.com>
2019-03-14 20:31:50 +0800

    finish c5

--- c5.py
+++ c5.py
@@ -2,11 +2,12 @@ import pickle
 import urllib.request
 
 content = urllib.request.urlopen('http://www.pythonchallenge.com/pc/def/banner.p').read()
-fhand = open('pickl.p', 'wb')
-fhand.write(content)
-fhand.close()
+cc = pickle.loads(content)
 
-fhand = open('pickl.p', 'rb')
-cc = pickle.load(fhand)
-print(cc)
-fhand.close()
+# cc为一个list，每个item也是一个list，每个list下若干个元组，
+#每个元组有两个数据，第一个为需要打印的字符，第二个为需要打印的个数。
+
+for item in cc:
+	for i in item:
+		print(i[0]*i[1], end = '')
+	print()
